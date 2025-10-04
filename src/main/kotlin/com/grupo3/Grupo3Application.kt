506a70d35880fa4b17a5709ce3a385c09b3d7615
Khav Kivar <sebastian.calderon@sansano.usm.cl>
2025-10-03 21:53:56 -0300

    Initial commit

---
+++ src/main/kotlin/com/grupo3/Grupo3Application.kt
@@ -0,0 +1,12 @@
+package com.grupo3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Grupo3Application
+
+fun main(args: Array<String>) {
+    runApplication<Grupo3Application>(*args)
+
+}
