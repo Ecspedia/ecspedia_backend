efeebafb51b6d9c5795ee65d3a32b68cd4ad1ca9
Felipe <lfazocar@users.noreply.github.com>
2025-10-31 23:31:37 -0300

    feat: add JWT authentication to security chain
    also move jwt and security related code to security folder

--- src/main/kotlin/com/grupo3/security/jwt/JwtTokenService.kt
+++ src/main/kotlin/com/grupo3/security/jwt/JwtTokenService.kt
@@ -1,18 +1,22 @@
-package com.grupo3.service
+package com.grupo3.security.jwt
 
 import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.exceptions.JWTVerificationException
+import com.auth0.jwt.interfaces.DecodedJWT
 import com.grupo3.model.User
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.util.Date
 
 @Service
-class JwtService(
+class JwtTokenService(
     @Value("\${jwt.secret}") private val secret: String,
     @Value("\${jwt.expiration}") private val expiration: Long
     ) {
     private val algorithm: Algorithm = Algorithm.HMAC256(secret)
+    private val verifier: JWTVerifier = JWT.require(algorithm).build()
 
     fun generateToken(user: User): String {
         return JWT.create()
@@ -21,5 +25,13 @@ class JwtService(
             .withIssuedAt(Date())
             .withExpiresAt(Date(System.currentTimeMillis() + expiration))
             .sign(algorithm)
+    }
+
+    fun validateToken(token: String): DecodedJWT? {
+        return try {
+            verifier.verify(token)
+        } catch (verificationEx: JWTVerificationException) {
+            null
+        }
     }
 }
